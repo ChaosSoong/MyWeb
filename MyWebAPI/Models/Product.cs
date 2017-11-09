4360d90d14facd715291ea3b6ece42fea45d37a1
nst <13313020563@163.com>
2017-11-09 11:15:45 +0800

    myMall init oh yeah

---
+++ MyWebAPI/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebAPI.Models
+{
+    public class Product
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public string image { get; set; }
+    }
+}
